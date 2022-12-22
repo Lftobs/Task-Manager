32b0344e6a330096296f355edc694b1510560a1d
Lftobs <106287048+Lftobs@users.noreply.github.com>
2022-12-22 05:58:48 +0100

    Ud

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 class Item(BaseModel):
@@ -7,52 +7,12 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
 
-class User(BaseModel):
-    name: str
-    age: int
-    nick_name: str
-
 app = FastAPI()
 
 @app.get("/")
 async def home():
-    return {"mes": "hi"}
+    return {"message": "hello world!"}
     
 @app.post("/items/")
 async def create_item(item:Item):
     return item
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/tobs/{fn}")
-async def name(fn: int):
-    return {"firstname": fn}
-
-@app.put("/tobs/{fn}")
-async def name(fn: int, q: str | None = None, item: Item | None = None):
-    results = {"item_id": fn}
-    if q:
-        results.update({"q": q})
-    if item:
-        results.update({"item": item})
-    return results
-
-@app.put("/items/{item_id}")
-async def update_item(
-    *,
-    item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
-    q: str | None = None,
-    item: Item | None = None,
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    if item:
-        results.update({"item": item})
-    return results
-
-@app.post("/prat/")
-async def user(user: User):
-    return user
