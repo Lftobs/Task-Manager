f9bce8e51873a3d4669febe299c59d02088d43d7
Lftobs <106287048+Lftobs@users.noreply.github.com>
2023-02-14 15:34:28 +0100

    testing cascade

--- app/routes.py
+++ app/routes.py
@@ -99,7 +99,7 @@ async def refresh_token(Authorize: AuthJWT = Depends()):
 
 @auth.delete('/all-users')
 async def all(db: Session = Depends(get_db)):
-    users = db.query(User).all()
-    for u in users:
-        db.delete(u)
+    users = db.query(User).filter(User.id==13).first()
+    #for u in users:
+    db.delete(user)
     return users
